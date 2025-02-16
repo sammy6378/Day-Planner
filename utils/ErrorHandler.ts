95a46b38e54455c77345a09be4e6cf675a6c2de4
sammy6378 <samuel202mwangi@gmail.com>
2025-02-16 12:47:53 +0300

    Fix type casting for Error.captureStackTrace in ErrorHandler class

--- utils/ErrorHandler.ts
+++ utils/ErrorHandler.ts
@@ -7,7 +7,7 @@ class ErrorHandler extends Error {
     constructor(message: string, statusCode: number) {
         super(message);
         this.statusCode = statusCode;
-        Error.captureStackTrace(this, this.constructor);
+        (Error as any).captureStackTrace(this, this.constructor);
     }
 }
 
