71d4c0764e243b6ca326bb3f7ef44c75a72ade12
sammy6378 <samuel202mwangi@gmail.com>
2025-02-16 12:29:57 +0300

    Add database connection on server start

--- server.ts
+++ server.ts
@@ -1,7 +1,9 @@
 import { app } from "./app";
+import { connectDB } from "./utils/connect";
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    connectDB();
 })
